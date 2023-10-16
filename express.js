125213234c0618eb358da6c5d30440a1bbdd88fc
Alejandro <66677531+Abanoral@users.noreply.github.com>
2023-10-16 21:10:58 +0200

    cambios iniciales

--- express.js
+++ express.js
@@ -1,7 +1,51 @@
 // Ejercicio 2: crear servidor HTTP con Express
+const express = require('express')
+const fs = require('node:fs')
+const app = express()
+
+const PORT = process.env.PORT ?? 1234
+
+app.use(express.json())
+app.use(express.static('assets'))
+
+app.all('/', (req, res) => {
+  if (req.method === 'GET') {
+    return res.send('<h1>¡Hola mundo!</h1>')
+  }
+  res.status(405)
+})
+
+app.get('./logo.webp', (req, res) => {
+  if (req.method === 'GET') {
+    fs.readFile('./logo.webp', (err, data) => {
+      if (err) {
+        return res.status(500).send('<h1>Internal server error</h1>')
+      } else {
+        return res.send(data)
+      }
+    })
+  }
+  res.status(405)
+})
+
+app.all('/404', (req, res) => {
+  if (req.method === 'GET') { return res.status(404).send('<h1>404</h1>') }
+  res.status(405)
+})
+
+app.all('/contacto', (req, res) => {
+  if (req.method === 'POST') { return res.status(201).json(req.body) }
+  res.status(405)
+})
+
+app.use((req, res, next) => {
+  res.status(404)
+  next()
+})
+
 function startServer () {
-  // const server = app.listen(...)
-  // return server
+  const server = app.listen(PORT)
+  return server
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,122 @@
 // Ejercicio 1: crear servidor HTTP con Node
+const http = require('node:http')
+const fs = require('node:fs')
+
+const processReq = (req, res) => {
+  const { method, url } = req
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  switch (method) {
+    case 'GET': {
+      switch (url) {
+        case '/': {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          res.end('<h1>¡Hola mundo!</h1>')
+          break
+        }
+        case '/logo.webp': {
+          fs.readFile('./assets/logo.webp', (err, data) => {
+            if (err) {
+              res.statusCode = 500
+              res.end('<h1>Internal server error</h1>')
+            } else {
+              res.setHeader('Content-Type', 'image/webp')
+              res.end(data)
+            }
+          })
+          break
+        }
+        case '/404': {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          res.end('<h1>404</h1>')
+          break
+        }
+        default: {
+          res.statusCode = 404
+          break
+        }
+      }
+      break
+    }
+    case 'POST': {
+      switch (url) {
+        case '/contacto': {
+          let body = ''
+          res.on('data', (chunk) => {
+            body += chunk.toString()
+          })
+          res.on('end', () => {
+            const data = JSON.parse(body)
+            res.writeHead(201, {
+              'Content-Type': 'application/json; charset=utf-8'
+            })
+            res.end(JSON.stringify(data))
+          })
+          break
+        }
+        default: {
+          res.statusCode = 404
+          break
+        }
+      }
+      break
+    }
+    default: {
+      res.statusCode = 405
+    }
+  }
+}
+
+const processToReq = (req, res) => {
+  const { method, url } = req
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  switch (method) {
+    case 'GET': {
+      switch (url) {
+        case '/': {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          res.end('<h1>¡Hola mundo!</h1>')
+          break
+        }
+        case '/logo.webp': {
+          fs.readFile('./assets/logo.webp', (err, data) => {
+            if (err) {
+              res.statusCode = 500
+              res.end('<h1>Internal server error</h1>')
+            } else {
+              res.setHeader('Content-Type', 'image/webp')
+              res.end(data)
+            }
+          })
+          break
+        }
+        case '/404': {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          res.end('<h1>404</h1>')
+          break
+        }
+        default: {
+          res.statusCode = 404
+          break
+        }
+      }
+      break
+    }
+    default: {
+      res.statusCode = 405
+    }
+  }
+}
+
+const port = process.env.PORT ?? 1234
+
 function startServer () {
-  // return server
+  const server = http.createServer(processToReq)
+
+  server.listen(port, () => {
+    const { port } = server.address()
+    console.log(`server listening on port http://localhost:${port}/`)
+  })
+  return server
 }
 
 module.exports = {
